1ff6397c17f0c4f82490bc869dac6277f9ac26d7
d0rianb <dorian.beauchesne@icloud.com>
2020-10-07 12:02:56 +0200

    Update README.md

--- README.md
+++ README.md
@@ -10,18 +10,18 @@ Other screenshot are available [here](docs/images)
 - Windows :
     - Execute `main.py` with `python 3`
 
-### Commandes
-Keybing can be edit in the game preferences.
-Default keybinding is :
-- __Se déplacer__  : Z, Q, S, D (or directionnal arrrows)
-- __Tirer__ : Left Clic
+### Commands
+Keybind can be edit in the game preferences.
+Default keybindings is :
+- __Move__  : Z, Q, S, D (or directional arrows)
+- __Shoot__ : Left Clic
 - __Dash__ : Left Shift
-- __Recharger__ : R
+- __Reload__ : R
 - __Help__ : H
 - __Melee__ : E
 
 ### To-Do
-- Health regain
+- Refactor code with types
 - Add textures
 - Server
 - Chat In-Game
--- client/collectible.py
+++ client/collectible.py
@@ -5,6 +5,7 @@ from object.color import Color
 
 BG_COLOR = Color((241, 231, 220))
 
+
 class Collectible:
     def __init__(self, id, x, y, type, env):
         self.id = id
@@ -16,14 +17,15 @@ class Collectible:
         self.size = 25
         self.is_active = True
 
-
     def on_collect(self, player):
         print(f'{player.name} collected {self.type}')
         self.is_active = False
 
     def render(self):
         if not self.is_active: return
-        self.env.rendering_stack.append(RenderedObject('oval', self.x, self.y, x2=self.x+self.size, y2=self.y+self.size, color=self.color, zIndex=4))
+        self.env.rendering_stack.append(
+            RenderedObject('oval', self.x, self.y, x2=self.x + self.size, y2=self.y + self.size, color=self.color,
+                           zIndex=4))
 
 
 class Heal(Collectible):
@@ -43,7 +45,7 @@ class LandMine(Collectible):
         super().__init__(id, x, y, 'LandMine', env)
         self.damage = damage
         self.initial_color = Color((153, 29, 62))
-        self.color_speed = 1/60
+        self.color_speed = 1 / 60
 
     def on_collect(self, player):
         if self.is_active:
@@ -53,9 +55,10 @@ class LandMine(Collectible):
                 player.dead()
             self.is_active = False
 
-
     def render(self):
         if not self.is_active: return
-        alpha = 1-1/2*abs(math.cos(self.env.tick*self.color_speed))
+        alpha = 1 - 1 / 2 * abs(math.cos(self.env.tick * self.color_speed))
         self.color = Color.blend(self.initial_color, BG_COLOR, alpha).to_hex()
-        self.env.rendering_stack.append(RenderedObject('oval', self.x, self.y, x2=self.x+self.size, y2=self.y+self.size, color=self.color, zIndex=4))
+        self.env.rendering_stack.append(
+            RenderedObject('oval', self.x, self.y, x2=self.x + self.size, y2=self.y + self.size, color=self.color,
+                           zIndex=4))
